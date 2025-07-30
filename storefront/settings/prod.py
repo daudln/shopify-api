02f6755c40852166a3628b01e708848a91c8bad4
Daud Namayala <83674818+daudln@users.noreply.github.com>
2025-07-30 15:51:40 +0300

    Update prod.py

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 from .common import *  # noqa: F403
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [host for host in os.environ.get("ALLOWED_HOSTS").split(",") if host]
 
