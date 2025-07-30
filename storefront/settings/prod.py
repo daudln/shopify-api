421732d451d66c05f595317f481807c8b96d2498
Daud Namayala <83674818+daudln@users.noreply.github.com>
2025-07-30 15:19:07 +0300

    Update prod.py

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -9,10 +9,10 @@ ALLOWED_HOSTS = [host for host in os.environ.get("ALLOWED_HOSTS").split(",") if 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware"] # noqa: F405
